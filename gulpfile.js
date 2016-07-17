f4c96801e42a096a16d6e4d067975f16d6b30d7d
Jack Polgar <jack@polgar.id.au>
2016-07-17 19:18:10 +1000

    Minification!

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-var gulp  = require('gulp'),
-    sass  = require('gulp-sass');
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    rename     = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCss   = require('gulp-clean-css');
 
-var sassPaths = [
-  'node_modules'
-];
+var sassOptions = {
+  includePaths: [
+    'node_modules'
+  ],
+  precision: 6,
+  sourceComments: false,
+  outputStyle: 'expanded'
+}
 
 gulp.task('watch', function () {
   gulp.watch('scss/**/*.scss', ['sass']);
 });
 
+// Compiles and minifies Sass, includes a sourcemap for the minified stylesheet.
 gulp.task('sass', function () {
   gulp.src('scss/draper.scss')
-      .pipe(sass({includePaths: sassPaths}).on('error', sass.logError))
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      .pipe(gulp.dest('dist/'))
+      .pipe(rename({extname: '.min.css'}))
+      .pipe(sourcemaps.init())
+      .pipe(cleanCss())
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('dist/'));
 });
